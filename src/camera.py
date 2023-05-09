e94130b7b11f14e7e41c334b28825c75eaf272fe
k.akt <klimbers.high@gmail.com~>
2023-05-10 08:30:40 +0900

    Make it possible to change camera id by path parameter.

--- src/camera.py
+++ src/camera.py
@@ -7,21 +7,25 @@ class Camera:
     video = None
     thread: threading.Thread = None
     image: npt.NDArray[np.uint8] = None
-    event: threading.Event = None
+    event_get_frame: threading.Event = None
+    event_stop_thread: threading.Event = None
     last_index: int = None
     
     def __init__(self, index = 0) -> None:
         if Camera.video is None:
             Camera.thread = threading.Thread(target=Camera.frames, kwargs={"index": index})
             Camera.thread.start()
-            Camera.event = threading.Event()
+            Camera.event_get_frame = threading.Event()
+            Camera.event_stop_thread = threading.Event()
     
-    def __del__(self) -> None:
+    def stop() -> None:
+        Camera.event_stop_thread.set()
         Camera.video.release()
+        Camera.video = None
     
     def get_frame(self):
-        Camera.event.wait()
-        Camera.event.clear()
+        Camera.event_get_frame.wait()
+        Camera.event_get_frame.clear()
         if Camera.image is None:
             return None
         _, encoded = cv2.imencode('.jpg', Camera.image)
@@ -31,7 +35,10 @@ class Camera:
     @staticmethod
     def frames(index: int):
         Camera.video = cv2.VideoCapture(index)
+        Camera.last_index = index
         while True:
+            if Camera.event_stop_thread.is_set():
+                break
             _, image = Camera.video.read()
             Camera.image = image
-            Camera.event.set()
+            Camera.event_get_frame.set()
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from fastapi.responses import PlainTextResponse, StreamingResponse
 from fastapi.requests import Request
 from camera import Camera
 
-allowed_hosts: List[str] = ["192.168.0.132"]
+allowed_hosts: List[str] = ["192.168.0.2"]
 
 app = FastAPI()
 
@@ -16,12 +16,23 @@ def get_httpContent_generator(camera: Camera):
             continue
         yield (b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+def isCameraIdChanged(camera_id: int) -> bool:
+    return Camera.last_index != camera_id and Camera.thread is not None
+
+def get_camera_response(camera_id: int, client_host: str):
+    if isCameraIdChanged(camera_id):
+        Camera.stop()
+    if client_host in allowed_hosts:
+        return StreamingResponse(get_httpContent_generator(Camera(camera_id)), media_type='multipart/x-mixed-replace; boundary=frame')
+    return PlainTextResponse(status_code=400, content="Access Dinied.")
+
 @app.get("/", response_class=PlainTextResponse)
 async def index(request: Request):
-    if request.client.host in allowed_hosts:
-        return StreamingResponse(get_httpContent_generator(Camera()), media_type='multipart/x-mixed-replace; boundary=frame')
+    return get_camera_response(camera_id=0, client_host=request.client.host)
 
-    return PlainTextResponse(status_code=400, content="Access Dinied.")
+@app.get("/{camera_id}", response_class=PlainTextResponse)
+async def index_camera_id(camera_id: int, request: Request):
+    return get_camera_response(camera_id, client_host=request.client.host)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.0.2", port=80)
